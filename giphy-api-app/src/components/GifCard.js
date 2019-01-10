5e73d789a0249bab0a678b79e8c1b027997b834c
Leo <leo297747@gmail.com>
2019-01-10 11:19:00 -0500

    Changed one line in GifCard

--- giphy-api-app/src/components/GifCard.js
+++ giphy-api-app/src/components/GifCard.js
@@ -4,7 +4,6 @@ class GifCard extends Component {
     render() {
         return (
             <div className="GifCard">
-                <p>Adding test</p>
                 <p>This is where we want to render a GifCard</p>
             </div>
         );
