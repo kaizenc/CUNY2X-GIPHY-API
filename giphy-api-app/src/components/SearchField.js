51787ab65b7ca8a57e1ea928abf0355c60996515
Leo <leo297747@gmail.com>
2019-01-10 11:53:31 -0500

    Added search && handle inputs

--- giphy-api-app/src/components/SearchField.js
+++ giphy-api-app/src/components/SearchField.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import GifCard from './GifCard';
+let variables = require('../variables');
 
 class SearchField extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: "",
+            input: "surprised pikachu",
+            data: [],
         };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+    handleSearch = () => {
+        this.fetchGifData();
+        this.setState({ input: "", })
+    }
+
+    sanitizeInput = (input) => {
+        input = input.toUpperCase();
+        let split_input = input.split(" ");
+        let output = split_input.join('+');
+        return (output);
+    }
+
+    fetchGifData() {
+        let API_KEY = variables.API_KEY;
+        let SEARCH_TERM = this.sanitizeInput(this.state.input);
+        let url = "http://api.giphy.com/v1/gifs/search?q=" + SEARCH_TERM + "&api_key=" + API_KEY;
+        console.log(url);
+        
+        axios.get(url)
+            .then(response => {
+                var result = response.data.map();
+                this.setState({data: result});
+            }).catch(err => { console.log(err); });
     }
 
     render() {
         return (
             <div className="SearchField">
+                Search:&nbsp;
+                <input type="text" value={this.state.input} onChange={this.handleChange}></input>
+                <button onClick={this.handleSearch}>
+                    Search
+                </button>
                 <GifCard/>
             </div>
         );
